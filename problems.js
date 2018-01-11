6a9bf043b7355f9bbd3b35d80698437c8ce2cc99
sochetanie <allagorik2901@gmail.com>
2018-01-11 11:59:53 -0500

    new solutions

--- problems.js
+++ problems.js
@@ -1,5 +1,6 @@
 // #1
-let code = '(a(((a))a(ab)))' //=> return true
+let code = '(a(((a))a(ab)))' 
+// result return => true
 
 let tempArr = []
 function checkingCodeforParenthesis(code) {
@@ -21,42 +22,49 @@ function checkingCodeforParenthesis(code) {
 checkingCodeforParenthesis(code)
 
 
+
 // #2
-let arr1 = [2,6,90,456,23,5,9,1,2,3,4]
+let arr1 = [2,6,90,456,23,5,9,1,2,3,4,56,98,36,987,11,765]
 let arr2 = [23,6,4,765,4,1,2]
-// => [ 2, 6, 23 ]
+// result => [2, 6, 23, 1, 4, 765]
 
 let newArr = []
-
 function commonNum(arr2,arr1) {
-  for(let i=0; i<arr1.length; i++) {
-    for(let j=0; j<arr2.length; j++) {
-      if (arr2[i] === arr2[j]) {
-        if(!newArr.includes(arr1[i])) {
-          newArr.push(arr1[i])
+  for(let i=0; i<arr2.length; i++) {
+    for(let j=0; j<arr1.length; j++) {
+      if (arr2[i] === arr1[j]) {
+        if(!newArr.includes(arr2[i])) {
+          newArr.push(arr2[i])
         }
       }
     }
   }
   return newArr
 }
-commonNum(arr2,arr1)
+commonNum(arr1,arr2)
+
 
 
 // #3
-let strsArr = ['ABC','123','zyx','fs3']    //=> result ['A1zf','B2ys','C3x3']
+let arrOfStrs = ['ABC','123','zyx','fs3'] 
+//result => ['A1zf','B2ys','C3x3']
 
 let newArr = []
-
-function newStr(strsArr) {
-  for (let i =0; i< strsArr.map( e=> e.length ).reduce((a,b)=> Math.max(a,b)); i++){
-    let temp = ''
-    strsArr.forEach(e=>{
-      if(e[i]) temp += e[i]
-    })
-    newArr.push(temp)
-  }
-  return newArr
+function newStr(arrOfStr) {
+  
+  let splitArr = []
+  arrOfStrs.forEach( el => splitArr.push(el.split('')) )
+  
+  let tempStr = []
+  for(let j=1;j<splitArr.length;j++) {
+    for(let i=0;i<splitArr.length;i++) {
+      tempStr.push(splitArr[i].shift())
+      }
+      tempStr.join('')
+      newArr.push(tempStr)
+      tempStr = []
+    }
+    return newArr.map(el=>el.join(''))
 }
-newStr(strsArr)
+newStr(arrOfStrs)
 
